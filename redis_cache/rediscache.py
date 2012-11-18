1972af57c61e88ad85944d3e132def572d52cd39
Sam Zaydel <sam.zaydel@inktank.com>
2012-11-18 07:32:52 -0800

    Added a connection Class/Object and key hashing option
    Created a separate class to establish connection to redis. Class makes
    it a bit easier to create connections where custom settings are
    required, such as DB #, host, port. Also added an option to generate a
    md5 digest of the pickle object generated as a result of pickling
    arguments to a function. This reduces chance of collisions in key
    namespace and makes for consistent length of keys.

--- redis_cache/rediscache.py
+++ redis_cache/rediscache.py
@@ -5,10 +5,25 @@ from functools import wraps
 import pickle
 import json
 import base64
+import hashlib
 import redis
 
-connection = redis.StrictRedis()
+#connection = redis.StrictRedis()
 
+class RedisConnect(object):
+    '''
+    A simple object to store and pass database connection information.
+    This makes the Simple Cache class a little more flexible, for cases
+    where redis connection configuration needs customizing.
+    '''
+
+    def __init__(self,host=None,port=None,db=0):
+        self.host = host if host else 'localhost'
+        self.port = port if port else 6379
+        self.db = db
+
+    def connect(self):
+        return redis.StrictRedis(host=self.host,port=self.port,db=self.db)
 
 class CacheMissException(Exception):
     pass
@@ -20,27 +35,36 @@ class ExpiredKeyException(Exception):
 
 class SimpleCache(object):
 
-    def __init__(self, limit=1000, expire=60 * 60 * 24):
+    def __init__(self, limit=1000, expire=60 * 60 * 24, hashkeys=False):
         self.limit = limit  # No of json encoded strings to cache
         self.expire = expire  # Time to keys to expire in seconds
+        ##  Create a connection to redis using custom settings.
+        ## Example, using custom port and db:
+        ## self.connection = RedisConnect(host='localhost',port=8778,db=10).connect()
+        self.connection = RedisConnect().connect()
+
+        ## There may be instances where we want to create hashes to reduce
+        ## a chance of key collisions. An unlikely event, but possible under
+        ## particular use cases. Keys will also be of a consistent length.
+        self.hashkeys = hashkeys
 
     def make_key(self, key):
         return "SimpleCache-%s::%s" % (id(self), key)
 
     def get_set_name(self):
         return "SimpleCache-%s-keys" % id(self)
-        
+
     def store(self, key, value, expire=None):
         """ Stores a value after checking for space constraints and freeing up space if required """
         key = to_unicode(key)
         value = to_unicode(value)
         set_name = self.get_set_name()
-    
-        while connection.scard(set_name) >= self.limit:
-            del_key = connection.spop(set_name)
-            connection.delete(self.make_key(del_key))
 
-        pipe = connection.pipeline()
+        while self.connection.scard(set_name) >= self.limit:
+            del_key = self.connection.spop(set_name)
+            self.connection.delete(self.make_key(del_key))
+
+        pipe = self.connection.pipeline()
         if expire is None:
             expire = self.expire
         pipe.setex(self.make_key(key), expire, value)
@@ -56,9 +80,9 @@ class SimpleCache(object):
     def get(self, key):
         key = to_unicode(key)
         if key in self:
-            val = connection.get(self.make_key(key))
+            val = self.connection.get(self.make_key(key))
             if val is None:  # expired key
-                connection.srem(self.get_set_name(), key)
+                self.connection.srem(self.get_set_name(), key)
                 raise ExpiredKeyException
             else:
                 return val
@@ -71,17 +95,17 @@ class SimpleCache(object):
         return pickle.loads(base64.b64decode(self.get(key)))
 
     def __contains__(self, key):
-        return connection.sismember(self.get_set_name(), key)
+        return self.connection.sismember(self.get_set_name(), key)
 
     def __len__(self):
-        return connection.scard(self.get_set_name())
+        return self.connection.scard(self.get_set_name())
 
     def keys(self):
-        return connection.smembers(self.get_set_name())
+        return self.connection.smembers(self.get_set_name())
 
     def flush(self):
         keys = self.keys()
-        pipe = connection.pipeline()
+        pipe = self.connection.pipeline()
         for key in keys:
             pipe.delete(key)
         pipe.delete(self.get_set_name())
@@ -94,18 +118,24 @@ def cache_it(limit=1000, expire=60 * 60 * 24):
     must be pickleable.
     """
     def decorator(function):
-        cache = SimpleCache(limit, expire)
-        
+        cache = SimpleCache(limit, expire, hashkeys=True)
+
         @wraps(function)
         def func(*args):
-            key = pickle.dumps(args)
+            ## Key will be either a md5 hash or just pickle object,
+            ## in the form of `function name`:`key`
+            if cache.hashkeys:
+                key = hashlib.md5(pickle.dumps(args)).hexdigest()
+            else:
+                key = pickle.dumps(args)
             cache_key = '%s:%s' % (function.__name__, key)
+            print 'This is my key:', cache_key
             if cache_key in cache:
                 try:
                     return cache.get_pickle(cache_key)
                 except (ExpiredKeyException, CacheMissException) as e:
                     pass
-        
+
             result = function(*args)
             cache.store_pickle(cache_key, result)
             return result
@@ -120,17 +150,18 @@ def cache_it_json(limit=1000, expire=60 * 60 * 24):
     """
     def decorator(function):
         cache = SimpleCache()
-        
+
         @wraps(function)
         def func(*args):
             key = json.dumps(args)
             cache_key = '%s:%s' % (function.__name__, key)
             if cache_key in cache:
                 try:
+                    print cache_key
                     return cache.get_json(cache_key)
                 except (ExpiredKeyException, CacheMissException) as e:
                     pass
-                
+
             result = function(*args)
             cache.store_json(cache_key, result)
             return result
