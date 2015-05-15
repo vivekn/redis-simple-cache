b38ed97ba6d5fd25efec44b89025e235dcaf0509
Jared Deckard <jared.deckard@gmail.com>
2015-05-14 20:19:54 -0500

    Use the "is" keyword to compare expire to None

--- redis_cache/rediscache.py
+++ redis_cache/rediscache.py
@@ -126,7 +126,7 @@ class SimpleCache(object):
         if expire is None:
             expire = self.expire
 
-        if (isinstance(expire, int) and expire <= 0) or (expire == None):
+        if (isinstance(expire, int) and expire <= 0) or (expire is None):
             pipe.set(self.make_key(key), value)
         else:
             pipe.setex(self.make_key(key), expire, value)
