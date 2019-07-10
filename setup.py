57287dd0afdfe37c02753ed227c94da5378932e7
Omer Hanetz <omer@nymhealth.com>
2019-07-10 14:20:15 +0300

    Updated version.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def openf(fname):
 
 setup(
     name="redis-simple-cache-3k",
-    version="0.0.6",
+    version="0.0.6.3",
     author="Vivek Narayanan, Flávio Juvenal, Sam Zaydel. Python 3 support by Omer Hanetz",
     author_email="flaviojuvenal@gmail.com",
     description="redis-simple-cache is a pythonic interface for creating a cache over redis. "
