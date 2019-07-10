932fe16a34c0646892d4edb76a5ec2b24ac2fd89
Omer Hanetz <omer@nymhealth.com>
2019-07-10 14:11:12 +0300

    Updated documentation and versioning before packaging.

--- setup.py
+++ setup.py
@@ -6,17 +6,18 @@ def openf(fname):
     return open(os.path.join(os.path.dirname(__file__), fname))
 
 setup(
-    name="redis-simple-cache",
+    name="redis-simple-cache-3k",
     version="0.0.6",
-    author="Vivek Narayanan, Flávio Juvenal, Sam Zaydel",
+    author="Vivek Narayanan, Flávio Juvenal, Sam Zaydel. Ported to Python 3 by Omer Hanetz",
     author_email="flaviojuvenal@gmail.com",
     description="redis-simple-cache is a pythonic interface for creating a cache over redis. "
                 "It provides simple decorators that can be added to any function to cache its return values. ",
     license="3-clause BSD",
     keywords="decorator decorators redis cache",
-    url="https://github.com/vivekn/redis-simple-cache",
+    url="https://github.com/ohanetz/redis-simple-cache-3k",
     packages=['redis_cache'],
     long_description=openf("README.md").read(),
+    long_description_content_type='text/markdown',
     install_requires=[line.strip() for line in openf("requirements.txt") if line.strip()],
     classifiers=[
         "Development Status :: 3 - Alpha",
